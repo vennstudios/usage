f4ae4e7909341e8398b6244a80fc62139fc1fa8b
Erik <Erik>
2015-10-01 21:20:15 +0200

    GUI semi-working. Buttons and labels. Data-read not working yet.

--- usage/controller.py
+++ usage/controller.py
@@ -1,33 +1,39 @@
 import psutil
 import model
-import view #will probably be removed
+import view
 
-def readUsage():
-	"""
-	Uses psutil to get CPU and RAM usage percentages
-	"""
-	cpuUsage = ('CPU', psutil.cpu_percent(interval=0.1, percpu=False))
-	memory = psutil.virtual_memory()
-	ramUsage = ('RAM', memory.percent)
+class Usage(object):
 
-	return [cpuUsage, ramUsage]
+	def __init__(self):
+		self.aModel=model.Model()
+		self.updateModel()
 
-def updateModel(modelObject):
-	"""
-	Uses readUsage to update the current model.data
-	"""
-	updateList = readUsage()
-	modelObject.data = {}
-	for item in updateList:
-		modelObject.data[item[0]] = item[1]
-		
+	def readUsage(self):
+		"""
+		Uses psutil to get CPU and RAM usage as list of tuples ('component', percentage).
+		"""
+		cpuUsage = ('CPU', psutil.cpu_percent(interval=0.1, percpu=False))
+		memory = psutil.virtual_memory()
+		ramUsage = ('RAM', memory.percent)
+
+		return [cpuUsage, ramUsage]
+
+	def updateModel(self):
+		"""
+		Uses readUsage to update the current model.data. Returns the same values as readUsage.
+		"""
+		updateList = self.readUsage()
+		self.aModel.data = {}
+		for item in updateList:
+			self.aModel.data[item[0]] = item[1]
+
+		return updateList
+			
 
 
-def renderView(modelObject):
-	print modelObject.data
+def main():
+	usageObj = Usage()
+	view.main(usageObj)
 
-
-if __name__ == "__main__":
-	aModel = model.Model()
-	updateModel(aModel)
-	renderView(aModel)
+if __name__ == '__main__':
+	main()
--- usage/view.py
+++ usage/view.py
@@ -1,1 +1,57 @@
-## Will probably be removed
+import sys
+import controller
+from PySide import QtGui, QtCore
+
+class Example(QtGui.QWidget):
+
+	def __init__(self,usageObj):
+		super(Example, self).__init__()
+		self.usageObj=usageObj
+		self.initUI()
+
+	def initUI(self):
+
+		ramLabel = QtGui.QLabel('RAM')
+		cpuLabel = QtGui.QLabel('CPU')
+
+		refreshBtn = QtGui.QPushButton('Refresh')
+		quitBtn = QtGui.QPushButton('Quit',self)
+
+		refreshBtn.setShortcut('R')
+		quitBtn.setShortcut('Q')
+
+		quitBtn.clicked.connect(QtCore.QCoreApplication.instance().quit)
+		refreshBtn.clicked.connect(self.initDataViz)
+		
+		grid = QtGui.QGridLayout()
+		grid.setSpacing(10)
+
+		grid.addWidget(ramLabel, 1, 0)
+		grid.addWidget(cpuLabel, 1, 1)
+
+		grid.addWidget(quitBtn,2,0)
+		grid.addWidget(refreshBtn,2,1)
+
+		self.setLayout(grid)
+
+		self.setGeometry(300,300,350,300)
+		self.setWindowTitle('Usage')
+		self.show()
+
+	
+	def initDataViz(self):
+		pass
+	# 	data = self.usageObj.updateModel()
+	# 	ramUsage = QtGui.QLabel(str(data[0][0]))
+	# 	cpuUsage = QtGui.QLabel(str(data[1][0]))
+
+
+
+def main(usageObj):
+
+	app = QtGui.QApplication(sys.argv)
+	ex = Example(usageObj)
+	sys.exit(app.exec_())
+
+if __name__ == '__main__':
+	main()
